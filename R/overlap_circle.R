d96a6472db67e90307d56f96a28d79080651a33f
RemPsyc <RemPsyc@users.noreply.github.com>
2022-02-16 15:30:32 -0500

    Made text output of overlap_circle invisible

--- R/overlap_circle.R
+++ R/overlap_circle.R
@@ -28,18 +28,18 @@ overlap_circle <- function(response, categories = c("Self", "Other")){
   scale = (c(1,2,3,4,5,6,7))
   overlap = (c(0,10,20,30,55,65,85))
   po <- round(approx(scale, overlap, xout = response)$y, digits=2) # po = Percentage overlap
-  plot <- VennDiagram::draw.pairwise.venn(area1 = 100,
-                                          area2 = 100,
-                                          cross.area = po,
-                                          category = categories,
-                                          cat.cex = 4,
-                                          cex = 2,
-                                          cat.pos = c(330, 30),
-                                          cat.dist = -.09,
-                                          lwd = 10,
-                                          ext.pos = 0,
-                                          ext.dist = -5,
-                                          sep.dist = 0.02,
-                                          label.col = c("white","black","white"))
-  plot
+  invisible(VennDiagram::draw.pairwise.venn(area1 = 100,
+                                    area2 = 100,
+                                    cross.area = po,
+                                    category = categories,
+                                    cat.cex = 4,
+                                    cex = 2,
+                                    cat.pos = c(330, 30),
+                                    cat.dist = -.09,
+                                    lwd = 10,
+                                    ext.pos = 0,
+                                    ext.dist = -5,
+                                    sep.dist = 0.02,
+                                    label.col = c("white","black","white")))
+
 }
