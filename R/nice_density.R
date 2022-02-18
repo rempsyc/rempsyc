b5321e2e30a03c61d959a32ccc380170b7197721
RemPsyc <RemPsyc@users.noreply.github.com>
2022-02-17 22:26:28 -0500

    fixed a warning message in a dplyr::mutate call causing the tests to fail (second)

--- R/nice_density.R
+++ R/nice_density.R
@@ -50,11 +50,10 @@ nice_density <- function(variable, group, data, colours, ytitle="Density", xtitl
         p = paste0('= ', p, " (Shapiro-Wilk)")    }
       sub("0", "", p)
     }
-    suppressWarnings(
-      dat_text <- data %>% group_by(.data[[group]]) %>%
+    dat_text <- data %>% group_by(.data[[group]]) %>%
         summarise(text=shapiro.test(.data[[variable]])$p.value) %>%
+        rowwise() %>%
         mutate(text=sprintf("italic('p')~'%s'", format.p(text)))
-    )
   }
   # Make plot
   ggplot(data, aes_string(x=variable, fill=group)) +
