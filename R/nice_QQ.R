9b116289947af40ea7928655cb5f92c2a4584dfa
RemPsyc <RemPsyc@users.noreply.github.com>
2022-02-17 22:18:48 -0500

    fixed a warning message in a dplyr::mutate call causing the tests to fail

--- R/nice_QQ.R
+++ R/nice_QQ.R
@@ -37,12 +37,11 @@ nice_QQ <- function(variable, group, data, colours, groups.labels=NULL, grid=TRU
         p = paste0('= ', p, " (Shapiro-Wilk)")    }
       sub("0", "", p)
     }
-    suppressWarnings(
-      dat_text <- data %>% group_by(.data[[group]]) %>%
+    dat_text <- data %>% group_by(.data[[group]]) %>%
         summarise(text=shapiro.test(.data[[variable]])$p.value) %>%
+        rowwise() %>%
         mutate(text=sprintf("italic('p')~'%s'", format.p(text)))
-    )
-  }
+    }
   # Make plot
   ggplot(data = data, mapping = aes_string(fill=group, sample=variable)) +
     qqplotr::stat_qq_band() +
