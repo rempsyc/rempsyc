63dd8c4bc197a0cc2817aed2f4b3c6b935f9a185
RemPsyc <RemPsyc@users.noreply.github.com>
2022-04-02 16:28:55 -0400

    new function: nice_normality

---
+++ R/nice_normality.R
@@ -0,0 +1,47 @@
+#' @title Easy normality check per group
+#'
+#' @description Easily make nice per-group density and QQ plots through a wrapper around the `ggplot2` and `qqplotr` packages.
+#'
+#' @param variable The dependent variable to be plotted.
+#' @param group The group by which to plot the variable.
+#' @param data The data frame
+#' @param colours Supports providing custom colours
+#' @param groups.labels How to label the groups?
+#' @param grid Logical, whether to keep the default background grid or not. APA style suggests not using a grid in the background, though in this case some may find it useful to more easily estimate the slopes of the different groups.
+#' @param shapiro Logical, whether to include the p-value from the Shapiro-Wilk test on the plot.
+#' @param title An optional title, if desired.
+#' @param ... Further arguments from `nice_qq()` and `nice_density()` to be passed to `nice_normality()`
+#'
+#' @keywords QQ plots, normality, density, distribution
+#' @export
+#' @examples
+#' # Make the basic plot
+#' nice_normality(data = iris,
+#'                variable = "Sepal.Length",
+#'                group = "Species")
+#'
+#' # Further customization
+#' nice_normality(data = iris,
+#'                variable = "Sepal.Length",
+#'                group = "Species",
+#'                colours = c("#00BA38", "#619CFF", "#F8766D"),
+#'                groups.labels = c("(a) Setosa", "(b) Versicolor", "(c) Virginica"),
+#'                grid = FALSE,
+#'                shapiro = TRUE,
+#'                title = NULL)
+#'
+#' @seealso
+#' Other functions useful in assumption testing: \code{\link{nice_assumptions}}, \code{\link{nice_density}}, \code{\link{nice_qq}}, \code{\link{nice_var}}, \code{\link{nice_varplot}}
+#'
+
+nice_normality <- function(data, variable, group, colours, groups.labels,
+                    grid=TRUE, shapiro=FALSE, title=variable, ...) {
+  plot.a <- nice_density(data = data, variable = variable, group = group, colours = colours,
+                         groups.labels = groups.labels, grid = grid, shapiro = shapiro,
+                         title = title, ...)
+  plot.a
+  plot.b <- nice_qq(data = data, variable = variable, group = group, colours = colours,
+                    groups.labels = groups.labels, grid = grid, shapiro = shapiro,
+                    title = title, ...)
+  see::plots(plot.a, plot.b)
+}
