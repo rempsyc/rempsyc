e9d095a9bbb112ff951ed5d3f02d65dd41b02b83
RemPsyc <RemPsyc@users.noreply.github.com>
2022-06-08 11:27:49 -0400

    added utils dependency to packageVersion

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname){
- packageStartupMessage("Welcome to rempsyc package version ", packageVersion("rempsyc"), "! For tutorials visit: https://remi-theriault.com/tutorials \n
+packageStartupMessage("Welcome to rempsyc package version ", utils::packageVersion("rempsyc"), "! For tutorials visit: https://remi-theriault.com/tutorials \n
 This package is under active development. For bug reports, support, or special requests, visit: \nhttps://github.com/rempsyc/rempsyc/issues")
 }
