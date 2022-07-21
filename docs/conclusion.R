0f74275b2908cff6941e9f1dc70fd11562060cb7
RemPsyc <RemPsyc@users.noreply.github.com>
2022-07-20 23:56:01 -0400

    Attempt no2 to fix R CMD check errors

---
+++ docs/conclusion.R
@@ -0,0 +1,5 @@
+# Ending message
+
+cat("### Thanks for checking in
+    
+Make sure to check out this page again if you use the code after a time or if you encounter errors, as I periodically update or improve the code. Feel free to contact me for comments, questions, or requests to improve this function at https://github.com/rempsyc/rempsyc/issues. See all tutorials here: https://remi-theriault.com/tutorials.")
