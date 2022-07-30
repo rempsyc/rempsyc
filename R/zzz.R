81d428fac5c6e0331f2626cc6dc25623e20c3a12
RemPsyc <RemPsyc@users.noreply.github.com>
2022-07-29 23:08:38 -0400

    Fixed nice_randomize - Added new images for tutorials, which we can reference later in tutorial

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
 "Tutorials: https://remi-theriault.com/tutorials
-Bug reports, support, or special requests:
-https://github.com/rempsyc/rempsyc/issues
+Bug report, support, special request: https://github.com/rempsyc/rempsyc/issues
 
-Suggested APA citation:
-Th\u00e9riault, R. (2022). rempsyc: Convenience functions ",
+Suggested APA citation: Th\u00e9riault, R. (2022). rempsyc: Convenience functions ",
 "for psychology (R package version ",
 utils::packageVersion("rempsyc"),
 ") [Computer software]. https://rempsyc.remi-theriault.com "
