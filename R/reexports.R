60d7b802ff3146c1ae47ceefa5398dc15cfa8a72
rempsyc <remi.theriault@mail.mcgill.ca>
2022-08-27 21:23:55 -0400

    updated docs for exports

--- R/reexports.R
+++ R/reexports.R
@@ -1,10 +1,14 @@
 #' @importFrom ggplot2 ggsave
 #' @export
 ggplot2::ggsave
+#' @export
 ggplot2::waiver
 #' @importFrom flextable save_as_docx save_as_html save_as_image save_as_pptx
 #' @export
 flextable::save_as_docx
+#' @export
 flextable::save_as_html
+#' @export
 flextable::save_as_image
+#' @export
 flextable::save_as_pptx
