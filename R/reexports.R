2c6811cf160ec14b9ab9991b14021c79ab8bc8ae
rempsyc <remi.theriault@mail.mcgill.ca>
2022-08-27 22:07:52 -0400

    corrected namespace reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,14 +1,23 @@
 #' @importFrom ggplot2 ggsave
 #' @export
 ggplot2::ggsave
+
+#' @importFrom ggplot2 waiver
 #' @export
 ggplot2::waiver
-#' @importFrom flextable save_as_docx save_as_html save_as_image save_as_pptx
+
+#' @importFrom flextable save_as_docx
 #' @export
 flextable::save_as_docx
+
+#' @importFrom flextable save_as_html
 #' @export
 flextable::save_as_html
+
+#' @importFrom flextable save_as_image
 #' @export
 flextable::save_as_image
+
+#' @importFrom flextable save_as_pptx
 #' @export
 flextable::save_as_pptx
