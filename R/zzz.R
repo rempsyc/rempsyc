60d5156f80c4303d543fe5230eef58dc1a9facd8
rempsyc <remi.theriault@mail.mcgill.ca>
2022-08-29 18:18:02 -0400

    updated citation formatting

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
 "Suggested APA citation: Th\u00e9riault, R. (2022). rempsyc: Convenience functions ",
-"for psychology (R package version ",
+"for psychology \n(R package version ",
 utils::packageVersion("rempsyc"),
 ") [Computer software]. https://rempsyc.remi-theriault.com "
   )
