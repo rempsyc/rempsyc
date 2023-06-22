e5f36a3ceecc1a5f8baff21b6225c706b24e5364
rempsyc <remi.theriault@mail.mcgill.ca>
2023-06-22 18:21:29 -0400

    skip_if_not_installed("correlation") skip_if_not_installed("openxlsx2")

--- tests/testthat/test-cormatrix_excel.R
+++ tests/testthat/test-cormatrix_excel.R
@@ -1,4 +1,7 @@
 test_that("cormatrix_excel", {
+  skip_if_not_installed("correlation")
+  skip_if_not_installed("openxlsx2")
+
   expect_snapshot(cormatrix_excel(mtcars, select = c("mpg", "cyl", "disp", "hp", "carb"),
                                   filename = "cormatrix1"))
   expect_snapshot(cormatrix_excel(iris, p_adjust = "none", filename = "cormatrix2"))
